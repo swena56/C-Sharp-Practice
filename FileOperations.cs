f0df14c674455f0c6ef1d387c515d1939c2e7d45
Andrew Swenson <swena56@gmail.com>
2015-10-11 20:38:20 -0500

    set background

--- FileOperations.cs
+++ FileOperations.cs
@@ -20,16 +20,71 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Win32;
 
 namespace C_Sharp_Practice
 {
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(Uri uri, Style style)
+        {
+            System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
+
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+
     public class FileOperations
     {
         public FileOperations()
         {
             Console.WriteLine("FileOperations constructor");
             addRegistryEntry();
+
+            //changeBackground(new Uri("http://t3.gstatic.com/images?q=tbn:ANd9GcS-0d56tCE7lbXKopMQTh_ugToAx0R7-9v5iGXfKQe18MJ9-wi1"));  //random image from the internet
+            changeBackground(new Uri("C:\\Users\\Public\\Pictures\\Sample Pictures\\Penguins.jpg"));
 
             //encryption and decryption
             Console.WriteLine("Please enter a password to use:");
@@ -55,17 +110,15 @@ namespace C_Sharp_Practice
             string value =  System.Convert.ToBase64String(plainTextBytes);
             Console.Write(value);
 
+            //Console.WriteLine("Execute Powershell code");
             // getShellWithPowerShell();
+
+            Console.WriteLine("Download file from the internet");
             using (WebClient client = new WebClient())
             {
-
                 //client.Credentials
-
-
                 client.DownloadFile("https://andy56-signup.herokuapp.com/robots.txt",
                                     @"c:\Users\Andy2014\h");
-
-                
             }
         }
 
@@ -115,9 +168,11 @@ namespace C_Sharp_Practice
         }
 
       
-        private void changeBackground()
+        private void changeBackground(Uri uri)
         {
-
+            Console.WriteLine("Set wallpaper");
+            //new Uri("C:\\Users\\Andy2014\\Pictures\\1322640054677.jpg")
+            Wallpaper.Set(uri, Wallpaper.Style.Stretched);
         }
 
         //Start-Process notepad -WindowStyle Hidden
